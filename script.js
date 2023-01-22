329205cdbe8d52f8e1ce319f4917e1c5012bd564
John Yang <john.yang8097@gmail.com>
2023-01-22 14:19:04 -0500

    create array iteration for dropList

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ let country_list = {
     "AUD" : "AU",
  }
  const dropList = Array.from(document.querySelectorAll(".drop-list select"))
+
+
+
+
+ dropList.forEach(element => {
+    for(currency_list in country_list) {
+        let optionTag = `<option value="${currency_list}">${currency_list}</option>`
+        element.insertAdjacentHTML("beforeend", optionTag);
+    }
+});
